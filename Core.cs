9cd7ec2e1efff6eb2b00587c571d0ab66791113e
end-4 <97237370+end-4@users.noreply.github.com>
2026-04-27 17:44:02 +0200

    fix warnings, update pkg readme

--- Core.cs
+++ Core.cs
@@ -8,7 +8,7 @@ namespace MoreRevamp {
     [BepInPlugin("com.github.end-4.moreRevamp", "MoreRevamp", "1.0.0")]
     public class Plugin : BaseUnityPlugin {
         internal static ManualLogSource Log;
-        public static Sprite SourceSprite;
+        public static Sprite? SourceSprite;
         public static float SourcePPU;
         public static string SOURCE_BUTTON_NAME = "Border";
 
@@ -25,11 +25,11 @@ namespace MoreRevamp {
         // Search for source sprite
         private void Update() {
             if (SourceSprite != null) return;
-            Image source = GameObject.Find(SOURCE_BUTTON_NAME)?.GetComponent<Image>();
-            if (source != null && source.sprite != null) {
-                SourceSprite = source.sprite;
-                SourcePPU = source.pixelsPerUnitMultiplier * 7 / 4; // title screen border is thicc
-            }
+            Image? source = GameObject.Find(SOURCE_BUTTON_NAME)?.GetComponent<Image>();
+            
+            if (source == null || source.sprite == null) return;
+            SourceSprite = source.sprite;
+            SourcePPU = source.pixelsPerUnitMultiplier * 7 / 4; // title screen border is thicc
         }
     }
 
@@ -57,11 +57,11 @@ namespace MoreRevamp {
                             path.Contains("Skippables") // In thank you for playing screen
                         )
                     )
-               ) return;
+            ) return;
             // Blacklist
             if (
                 __instance.name == "UpvoteButton" || __instance.name == "Downvote button" // Angry vote arrows
-               ) return;
+            ) return;
             // Whitelist
             if ((__instance.name == "Border" && path.Contains("ConfigurationMenu(Clone)")) // Configgy overlay
                 || __instance.GetComponent<Button>() != null && ( // These buttons
@@ -74,7 +74,7 @@ namespace MoreRevamp {
                     __instance.name == "RankIcon(Clone)" // Angry rank
                     || __instance.name == "SearchBar(Clone)" // Angry search
                 )
-               ) {
+            ) {
                 // Apply style while preserving size & color
                 RectTransform rect = __instance.rectTransform;
                 Vector2 originalSize = rect.sizeDelta;
